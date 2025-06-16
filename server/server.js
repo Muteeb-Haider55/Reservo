292d689049a43e44fbb45720303fe10453ceb77f
Muteeb Haider <muteebhdr@gmail.com>
2025-06-16 18:49:32 +0500

    backend setup and connect to DB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import connectDB from "./configs/db.js"
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js"
+
+connectDB()
 
 const app = express()
 app.use(cors())  // Enable Cross-Origin Resourse Sharing
 
-app.get('/',(req, res)=> res.send("Api is working"))
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+// api to listen clerck webhooks
+
+app.use("/api/clerk",clerkWebhooks);
+
+app.get('/',(req, res)=> res.send("Api is working fine hi"))
 
 const PORT = process.env.PORT || 3000;
 
+
 app.listen(PORT, ()=>console.log(`Server is running on port : ${PORT}`));
+  
